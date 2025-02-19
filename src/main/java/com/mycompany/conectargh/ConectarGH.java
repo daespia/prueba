33ad0b663c38f760c4fb82488e182ccf6865f8b6
despinosa <despinosa@iescomercio.com>
2025-02-19 17:44:42 +0100

    add main file

---
+++ src/main/java/com/mycompany/conectargh/ConectarGH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conectargh;
+
+/**
+ *
+ * @author ProfVespertino
+ */
+public class ConectarGH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
