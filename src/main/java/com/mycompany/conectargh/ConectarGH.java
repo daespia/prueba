6925efed193db804d12b52012abb8e23a3a3dbae
despinosa <despinosa@iescomercio.com>
2025-02-19 17:58:56 +0100

    change main file

--- src/main/java/com/mycompany/conectargh/ConectarGH.java
+++ src/main/java/com/mycompany/conectargh/ConectarGH.java
@@ -11,6 +11,6 @@ package com.mycompany.conectargh;
 public class ConectarGH {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo!");
     }
 }
